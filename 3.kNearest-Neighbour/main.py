257e3f213c801c6c8b47aec147c588c4e6914640
Asutosh <asutoshsahoo786@gmail.com>
2017-01-31 00:39:27 +0530

    kNearest-Neighbour from Scratch(Google Developers Tutorial)

---
+++ 3.kNearest-Neighbour/main.py
@@ -0,0 +1,18 @@
+from sklearn import datasets
+iris = datasets.load_iris()
+
+X = iris.data
+Y = iris.target
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = .5)
+
+from sklearn.neighbors import KNeighborsClassifier
+my_classifier = KNeighborsClassifier()
+
+my_classifier.fit(X_train, y_train)
+
+predictions = my_classifier.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print accuracy_score(y_test, predictions)
